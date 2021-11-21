5fd6b24c76f8bd16345d55f82719a14c9a1fdeba
RodrigoAN97 <rodrigo_neves97@outlook.com>
2021-11-21 08:39:18 -0300

    refactor: :recycle: no env

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  HTMLToCSSAuth: {
-    username: 'b0db88e2-240c-4e82-ab7e-ac3f3293a0c7',
-    password: '1e238faa-a7ad-4aa1-a5d5-90b21739782c',
-  },
-};
